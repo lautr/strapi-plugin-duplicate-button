7b7b26dd4fb7a4f397fb6002562265596f60986b
Florian Dupuis <floriandupuis1996@gmail.com>
2022-10-19 18:09:20 -0400

    Remove duplicate button on SingleType

--- admin/src/components/DuplicateButton/index.js
+++ admin/src/components/DuplicateButton/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useIntl } from 'react-intl';
-import { useHistory } from 'react-router-dom';
-import { Button } from '@strapi/design-system/Button';
-import Duplicate from '@strapi/icons/Duplicate';
-import { useCMEditViewDataManager } from '@strapi/helper-plugin';
-import usePluginsQueryParams  from '@strapi/admin/admin/src/content-manager/hooks/usePluginsQueryParams';
+import React from "react";
+import { useIntl } from "react-intl";
+import { useHistory } from "react-router-dom";
+import { Button } from "@strapi/design-system/Button";
+import Duplicate from "@strapi/icons/Duplicate";
+import { useCMEditViewDataManager } from "@strapi/helper-plugin";
+import usePluginsQueryParams from "@strapi/admin/admin/src/content-manager/hooks/usePluginsQueryParams";
 
 const DuplicateButton = () => {
-  const { modifiedData, layout } = useCMEditViewDataManager();
+  const { modifiedData, layout, isSingleType } = useCMEditViewDataManager();
 
   const {
     push,
@@ -22,25 +22,25 @@ const DuplicateButton = () => {
     push({
       pathname: copyPathname,
       state: { from: pathname },
-      search: pluginsQueryParams
+      search: pluginsQueryParams,
     });
-  }
+  };
 
   const content = {
-    id: 'duplicate-button.components.duplicate.button',
-    defaultMessage: 'Duplicate',
-  }
+    id: "duplicate-button.components.duplicate.button",
+    defaultMessage: "Duplicate",
+  };
 
+  if (isSingleType) return null;
   return (
     <>
-      {
-        modifiedData.id &&
+      {modifiedData.id && (
         <Button variant="secondary" startIcon={<Duplicate />} onClick={handleDuplicate}>
           {formatMessage(content)}
         </Button>
-      }
+      )}
     </>
-  )
-}
+  );
+};
 
-export default DuplicateButton
+export default DuplicateButton;
